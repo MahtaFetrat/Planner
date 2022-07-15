952a230611ee573210cd2fcf8303dc887ecd1091
Sadegh <59364741+SadeghMahmoudabadi@users.noreply.github.com>
2022-07-15 20:35:29 +0430

    Task colors

--- app/src/main/java/com/example/planner/ui/views/TasksAdapter.java
+++ app/src/main/java/com/example/planner/ui/views/TasksAdapter.java
@@ -1,15 +1,18 @@
 package com.example.planner.ui.views;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.planner.R;
+import com.example.planner.model.PriorityLevel;
 import com.example.planner.model.Task;
 
 import java.util.List;
@@ -36,6 +39,19 @@ public class TasksAdapter extends RecyclerView.Adapter<TasksAdapter.TasksViewHol
     public void onBindViewHolder(@NonNull TasksViewHolder holder, int position) {
         holder.taskName.setText(tasks.get(position).getTitle());
         holder.taskDescription.setText(tasks.get(position).getDescription());
+        switch (tasks.get(position).getPriorityLevel()) {
+            case REGULAR:
+                holder.itemView.setBackgroundColor(Color.parseColor("#42F548"));
+                break;
+            case IMPORTANT:
+                holder.itemView.setBackgroundColor(Color.parseColor("#F5f242"));
+                break;
+            case ESSENTIAL:
+                holder.itemView.setBackgroundColor(Color.parseColor("#F84949"));
+                break;
+            default:
+                break;
+        }
     }
 
     @Override
@@ -52,11 +68,13 @@ public class TasksAdapter extends RecyclerView.Adapter<TasksAdapter.TasksViewHol
 
         TextView taskName;
         TextView taskDescription;
+        ConstraintLayout taskLayout;
 
         public TasksViewHolder(@NonNull View itemView) {
             super(itemView);
             taskName = itemView.findViewById(R.id.taskNameTextView);
             taskDescription = itemView.findViewById(R.id.taskDescriptionTextView);
+            taskLayout = itemView.findViewById(R.id.taskRowLayout);
         }
     }
 }
