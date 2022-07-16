c1813089a648a5ab681bcfde4dfa851e2339fd71
MahtaFetrat <77fetrat@gmail.com>
2022-07-16 21:45:42 +0430

    change task activity to fragment

--- app/src/main/java/com/example/planner/ui/views/TaskFragment.java
+++ app/src/main/java/com/example/planner/ui/views/TaskFragment.java
@@ -1,60 +1,42 @@
 package com.example.planner.ui.views;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.planner.R;
+import com.example.planner.ui.viewModels.TaskViewModel;
+import com.example.planner.ui.views.Adapters.TasksAdapter;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link TaskFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+
+
 public class TaskFragment extends Fragment {
+    private TaskViewModel viewModel;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private Button createNewTask;
+    private RecyclerView tasks;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private TasksAdapter tasksAdapter;
 
     public TaskFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment TaskFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static TaskFragment newInstance(String param1, String param2) {
-        TaskFragment fragment = new TaskFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -62,5 +44,41 @@ public class TaskFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_task, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        viewModel = new ViewModelProvider(requireActivity()).get(TaskViewModel.class);
+
+        findViewsById(view);
+        initializeListAdapter();
+        setOnClickListeners();
+        setViewModelObservers();
+    }
+
+    private void findViewsById(View view) {
+        createNewTask = view.findViewById(R.id.createNewTaskButton);
+        tasks = view.findViewById(R.id.tasksRecyclerView);
+    }
+
+    private void initializeListAdapter() {
+        tasksAdapter = new TasksAdapter(getActivity(), new ArrayList<>());
+        tasks.setAdapter(tasksAdapter);
+        tasks.setLayoutManager(new LinearLayoutManager(getActivity()));
+    }
+
+    private void setOnClickListeners() {
+        createNewTask.setOnClickListener(view -> {
+            Intent intent = new Intent(getActivity(), CreateTaskActivity.class);
+            startActivity(intent);
+        });
+    }
+
+    private void setViewModelObservers() {
+        viewModel.getAllTasks().observe(getViewLifecycleOwner(), allTasks -> {
+            tasksAdapter.updateList(allTasks);
+        });
     }
 }
