da7f5b4775b04615cecabd8be0ee89e02bb44239
MahtaFetrat <77fetrat@gmail.com>
2022-07-19 03:51:25 +0430

    add startup message

---
+++ app/src/main/java/com/example/planner/ui/views/StartupActivity.java
@@ -0,0 +1,63 @@
+package com.example.planner.ui.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.planner.R;
+import com.example.planner.model.Motivation;
+import com.example.planner.ui.viewModels.TaskViewModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StartupActivity extends AppCompatActivity {
+
+    ConstraintLayout startupLayout;
+    TextView startupSentenceTextView;
+    TaskViewModel viewModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_startup);
+
+        viewModel = new ViewModelProvider(this).get(TaskViewModel.class);
+
+        findViewsById();
+        setOnClickListeners();
+        setStartupSentence();
+    }
+
+    private void findViewsById() {
+        startupLayout = findViewById(R.id.startupLayout);
+        startupSentenceTextView = findViewById(R.id.startupSentenceTextView);
+    }
+
+    private void setOnClickListeners() {
+        startupLayout.setOnClickListener(view -> {
+            Intent intent = new Intent(this, MainActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+        });
+    }
+
+    private void setStartupSentence() {
+        viewModel.getAllMotivations().observe(this, new Observer<List<Motivation>>() {
+            @Override
+            public void onChanged(List<Motivation> motivations) {
+                if (!motivations.isEmpty()) {
+                    int randomIndex = (int)(Math.random() * motivations.size());
+                    Motivation motivation = motivations.get(randomIndex);
+                    startupSentenceTextView.setText(motivation.getSentence());
+                }
+            }
+        });
+    }
+}
